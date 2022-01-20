cee3d155245634d0c25c31f0a3cef27ee73f2af7
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-20 11:51:40 -0500

    built view to add a new reminder

---
+++ Shared/Views/AddTask.swift
@@ -0,0 +1,42 @@
+//
+//  AddTask.swift
+//  Reminders
+//
+//  Created by Vincent Cloutier on 2022-01-20.
+//
+
+import SwiftUI
+
+struct AddTask: View {
+    // New Task Details
+    @State private var description = ""
+    @State private var priority = TaskPriority.low
+    var body: some View {
+        // Add one because this will be used in a sheet
+        NavigationView {
+            VStack {
+                Form {
+                    TextField("Description", text: $description)
+                    Picker("Priority", selection: $priority) {
+                        Text(TaskPriority.low.rawValue).tag(TaskPriority.low)
+                        Text(TaskPriority.medium.rawValue).tag(TaskPriority.medium)
+                        Text(TaskPriority.high.rawValue).tag(TaskPriority.high)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationTitle("New Reminder")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Save") {
+                        saveTask()
+                    }
+                }
+            }
+        }
+    }
+    
+    func saveTask() {
+        
+    }
+}
