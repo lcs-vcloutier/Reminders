008055f0b5c2f4b75701cb02087d80a2e1cc5bd4
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-25 09:20:59 -0500

    added segmented filter picker

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -12,39 +12,57 @@ struct ContentView: View {
     @State private var showingAddTask = false
     @State var showingCompletedTasks = true
     @State var listShouldUpdate = false
+    @State private var selectedPriorityForVisibleTasks: VisibleTaskPriority = .all
     var body: some View {
         let _ = print("\(listShouldUpdate)")
-        List {
-            ForEach(store.tasks) { task in
-                if showingCompletedTasks {
-                    TaskCell(task: task, triggerListUpdate: .constant(true))
-                } else {
-                    if !task.completed {
-                        TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
+        
+        VStack {
+            Text("Filter by...")
+                .font(Font.caption.smallCaps())
+                .foregroundColor(.secondary)
+                .padding(.horizontal)
+            
+            Picker("Priority", selection: $selectedPriorityForVisibleTasks) {
+                Text(VisibleTaskPriority.all.rawValue).tag(VisibleTaskPriority.all)
+                Text(VisibleTaskPriority.low.rawValue).tag(VisibleTaskPriority.low)
+                Text(VisibleTaskPriority.medium.rawValue).tag(VisibleTaskPriority.medium)
+                Text(VisibleTaskPriority.high.rawValue).tag(VisibleTaskPriority.high)
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+            
+            List {
+                ForEach(store.tasks) { task in
+                    if showingCompletedTasks {
+                        TaskCell(task: task, triggerListUpdate: .constant(true))
+                    } else {
+                        if !task.completed {
+                            TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
+                        }
+                    }
+                }
+                .onDelete(perform: store.deleteItems)
+                .onMove(perform: store.moveItems)
+            }
+            .navigationTitle("Reminders")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Add") {
+                        showingAddTask = true
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Button(showingCompletedTasks ? "Hide Completed Tasks" : "Show Completed Tasks") {
+                        showingCompletedTasks.toggle()
                     }
                 }
             }
-            .onDelete(perform: store.deleteItems)
-            .onMove(perform: store.moveItems)
-        }
-        .navigationTitle("Reminders")
-        .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                Button("Add") {
-                    showingAddTask = true
-                }
+            .sheet(isPresented: $showingAddTask) {
+                AddTask(store: store, showing: $showingAddTask)
             }
-            ToolbarItem(placement: .navigationBarLeading) {
-                EditButton()
-            }
-            ToolbarItem(placement: .bottomBar) {
-                Button(showingCompletedTasks ? "Hide Completed Tasks" : "Show Completed Tasks") {
-                    showingCompletedTasks.toggle()
-                }
-            }
-        }
-        .sheet(isPresented: $showingAddTask) {
-            AddTask(store: store, showing: $showingAddTask)
         }
     }
 }
